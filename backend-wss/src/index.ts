d50fe1329b3b2c971ab49d020c39685f514dfeaa
Swapnil Shah <swapnilshah5889@gmail.com>
2025-02-27 20:41:11 -0600

    Simple chat

--- backend-wss/src/index.ts
+++ backend-wss/src/index.ts
@@ -1,10 +1,40 @@
-import { WebSocketServer } from "ws";
+import { WebSocketServer, WebSocket } from "ws";
 
 const wss: WebSocketServer = new WebSocketServer({ port: 8080 });
 
-let userCount: number = 0;
+type User = {
+    id: number,
+    name: string,
+    socket: WebSocket
+}
+
+let userList: User[] = [];
 
 wss.on("connection", (socket: WebSocket) => {
-    userCount += 1;
-    console.log("user connected #" + userCount);
+
+    // On new connection - add the user
+    const newUser: User = {
+        id: userList.length,
+        name: "User#" + userList.length,
+        socket
+    };
+    userList.push(newUser);
+
+    console.log("user connected #" + newUser.id);
+
+    // Broadcast messages to other users
+    socket.on("message", (message) => {
+
+        userList.forEach((user: User) => {
+            if (user.id !== newUser.id) {
+                user.socket.send(user.name + ": " + message.toString())
+            };
+        });
+
+    });
+
+    // Remove user on disconnect
+    socket.on("disconnect", () => {
+        userList = userList.filter(u => u.socket !== socket);
+    });
 });
